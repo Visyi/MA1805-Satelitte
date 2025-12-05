783ba275ac4c279ada3f294e085979311bf837b8
Visyi <simonbahrani@gmail.com>
2025-12-05 00:05:02 +0000

    commit

--- sketch.js
+++ sketch.js
@@ -2,36 +2,50 @@ let bg;
 let overgrowth;
 let slider;
 let ambientsound;
+let started = false; // track if user has clicked
 
 function preload() {
   bg = loadImage('assets/bg.png');
   overgrowth = loadImage('assets/bgog.png');
-  ambientsound = loadSound('assets/forest.mp3')
+  ambientsound = loadSound('assets/forest.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ambientsound.loop(); // plays continuously
-  ambientsound.setVolume(0.20);
 
-  // Sets min-max and starting value
+  // Slider setup
   slider = createSlider(0, 255, 0);
   slider.position(40, 20);
-  slider.size(100)
-
+  slider.size(100);
 }
 
 function draw() {
   background(bg);
 
+  // Show "Click to start" until user interacts
+  if (!started) {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("Click to start", width / 2, height / 2);
+    return; // stop drawing rest until started
+  }
+
   // Get slider value
   let opacity = slider.value();
 
   if (opacity > 0) {
-    push();                // save current drawing style
-    tint(255, opacity);    // apply alpha
+    push();
+    tint(255, opacity);
     image(overgrowth, 0, 0, width, height);
-    pop();                 // restore drawing style (removes tint)
+    pop();
   }
+}
 
+function mousePressed() {
+  if (!started) {
+    started = true;
+    ambientsound.loop();
+    ambientsound.setVolume(0.20);
+  }
 }
